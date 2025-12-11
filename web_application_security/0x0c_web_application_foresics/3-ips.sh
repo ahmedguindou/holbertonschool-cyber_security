d16537bda1b5766911c4e5d8e260ae6094415327
ahmedguindou <guindouahmed@gmail.com>
2025-12-11 16:22:06 +0100

    foresics

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -3,4 +3,4 @@
 # Count distinct IP addresses that successfully accessed the system
 
 # Extract IPs from successful SSH logins, remove duplicates, and count
-grep "Accepted password" auth.log | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u | wc -l
+grep "Accepted password for root" auth.log | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u | wc -l
